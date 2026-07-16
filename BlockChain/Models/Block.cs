cf38a9dd333dd42b675cb5dc6b2d3ee3797e9cbf
Arsenii <arseniydorofienko@gmail.com>
2026-07-16 23:18:16 +0200

    Add project files.

---
+++ BlockChain/Models/Block.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlockChain.Models
+{
+    public class Block
+    {
+        public int Index { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public string Data { get; set; }
+        public string PrevHash { get; set; }
+
+        public string Hash { get; set; }
+
+        public Block(int index, DateTime timeStamp, string data, string prevHash)
+        {
+            Index = index;
+            TimeStamp = timeStamp;
+            Data = data;
+            PrevHash = prevHash;
+        }
+    }
+}
---
+++ BlockChain/Program.cs
@@ -0,0 +1,14 @@
+﻿using BlockChain.Services;
+
+var blockChainService = new BlockChainService();
+
+blockChainService.AddBlock("Mark sent 10 coins to John");
+blockChainService.AddBlock("John sent 5 coins to Alice");
+
+BlockChainDisplayService.DisplayBlockChain(blockChainService.Chain);
+BlockChainDisplayService.DisplayValidationResult(blockChainService.IsValid());
+Console.WriteLine(Environment.NewLine);
+
+blockChainService.Chain[2].Data = "John sent 50 coins to Alice"; // Tampering with the blockchain
+BlockChainDisplayService.DisplayBlockChain(blockChainService.Chain);
+BlockChainDisplayService.DisplayValidationResult(blockChainService.IsValid());
