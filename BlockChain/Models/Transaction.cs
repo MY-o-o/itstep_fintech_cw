1846ed0dffc60d3502809cbc7d9f0184dceaefe7
Arsenii <arseniydorofienko@gmail.com>
2026-07-23 18:52:14 +0200

    Create Transaction model

---
+++ BlockChain/Models/Transaction.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlockChain.Models
+{
+    public class Transaction
+    {
+        public Guid Id { get; set; }
+        public string From { get; set; }
+        public string To { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime TimeStamp { get; set; }
+
+        public Transaction(string from, string to, decimal amount)
+        {
+            Id = Guid.NewGuid();
+            From = from;
+            To = to;
+            Amount = amount;
+            TimeStamp = DateTime.UtcNow;
+        }
+
+        public string ToRowString()
+        {
+            return $"{Id}\t{From}\t{To}\t{Amount}\t{TimeStamp}";
+        }
+
+        public override string ToString()
+        {
+            return $"Transaction [Id={Id}, From={From}, To={To}, Amount={Amount}, TimeStamp={TimeStamp}]";
+        }
+    }
+}
